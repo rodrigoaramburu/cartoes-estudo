c276828b06f94985cbe33c12c32996eb33edca5a
Rodrigo Aramburu <rodrigoaramburu@gmail.com>
2022-07-08 15:30:07 -0300

    feature: adicionado inserir imagem no editor

--- resources/js/editor.js
+++ resources/js/editor.js
@@ -4,6 +4,9 @@ window.editor = function(content){
     return {
         wysiwyg: null,
         content: '',
+
+        fileImageInput: null,
+
         init: function() {
             this.content = content;
             this.$refs.wysiwyg.contentDocument.querySelector('head').innerHTML += `<style>
@@ -21,11 +24,34 @@ window.editor = function(content){
             this.$refs.wysiwyg.contentDocument.addEventListener('keyup', function(){
                 _this.content = _this.$refs.wysiwyg.contentDocument.body.innerHTML;
             });
+
+
+
         },
         format: function(cmd, param) {
             this.$refs.wysiwyg.contentDocument.execCommand(cmd, !1, param||null);
             this.content = this.$refs.wysiwyg.contentDocument.body.innerHTML;
         },
+
+        loadImage: function(){
+            this.fileImageInput = document.createElement('input');
+            this.fileImageInput.type="file";
+            
+            let _this = this;
+
+            this.fileImageInput.addEventListener('change', function(){
+                const reader = new FileReader();
+                reader.onload = (event) => {
+                    let data = event.target.result;
+                    _this.$refs.wysiwyg.contentDocument.body.innerHTML += `<img src="data:${_this.fileImageInput.files[0].type};base64,${btoa(data)}" class="display: inline-block">`;
+                };
+                
+                reader.readAsBinaryString(_this.fileImageInput.files[0]);
+            });
+            
+            this.fileImageInput.click();
+
+        }
         
     }
 }
