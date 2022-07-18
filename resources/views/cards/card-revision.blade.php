d6dc725148694735229069f39433e623e135eacb
Rodrigo Aramburu <rodrigoaramburu@gmail.com>
2022-07-18 15:09:27 -0300

    feature: adicionado botão de editar cartão na tela de revisãp

--- resources/views/cards/card-revision.blade.php
+++ resources/views/cards/card-revision.blade.php
@@ -3,6 +3,8 @@
     
     <div class="flex flex-col justify-center items-center w-full h-full p-1" x-data="{flip: false}">
         
+    
+        
         @if($card)
             <h2 class="text-lg font-bold text-center mb-2">{{$card->deck()->name()}}</h2>
 
@@ -22,13 +24,23 @@
                 </div>
             </div>
 
-            <button @click="flip = true" :disabled="flip" class="flex gap-2 items-center bg-green-700 rounded-lg px-4 py-2 text-white disabled:bg-gray-400">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-                </svg>
-                Ver Resposta
-            </button>
+            <div class="flex gap-2 border-b border-gray-400 pb-3">
+                <button @click="flip = !flip"  class="flex gap-2 items-center bg-green-700 rounded-lg px-4 py-2 text-white disabled:bg-gray-400">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                    </svg>
+                    Ver <span x-show="!flip">Resposta</span> <span x-show="flip">Pergunta</span>
+                </button>
+                
+
+                <a href="{{route('cards.edit', $card->id())}}" class="flex gap-2 items-center bg-gray-700 rounded-lg px-4 py-2 text-white disabled:bg-gray-400">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                    </svg>
+                    Editar
+                </a>
+            </div>
 
             <form action="{{route('cards.nex-revision-store', $card->id())}}" method="POST" x-show="flip" class="flex gap-4 p-2 mt-2">
                 @csrf
